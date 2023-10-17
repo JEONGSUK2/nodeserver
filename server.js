86411bbe516b8bc2176b47e41326eead1b712d86
이정석 <dlwjdtjr3088@naver.com>
2023-10-17 18:12:28 +0900

    20231017 보드 최종본

--- server.js
+++ server.js
@@ -4,13 +4,18 @@ yarn add express - 서버 구동을 위한 라이브러리
 */ 
 const port = 5000;
  
-
 /* express를 세팅하는 방법 */
 const express = require('express'); // require 무엇을 불러오는 것
 const app = express()
 const dotenv = require('dotenv');
 
 dotenv.config();
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+const methodOverride = require('method-override');
+app.use(methodOverride('_method'));
+
 
 app.set('view engine', 'ejs');
 // ejs 기본세팅 (사용방법)
@@ -27,6 +32,7 @@ new MongoClient(url).connect().then((client)=>{
    db = client.db("board");
    sample = client.db("sample_training")
    console.log("db연결 완료!")
+   
    app.listen(process.env.SERVER_PORT, ()=>{
     console.log(`${process.env.SERVER_PORT}번호에서 서버 실행 중`)
 })
@@ -42,6 +48,10 @@ new MongoClient(url).connect().then((client)=>{
 */ 
 
 
+
+
+
+
 app.get('/', (req,res)=>{
     // res.send("Hello World");
     res.sendFile(__dirname + '/page/index.html')  
@@ -55,8 +65,16 @@ app.get('/about', (req,res)=>{
     //     content : "두번째 글"
     // })
 })
+
+
+
+
+
+
+
+
 app.get('/list', async (req,res)=>{
-    const result =  await db.collection("notice").find().toArray()
+    const result =  await db.collection("notice").find().skip(((req.params.id -1) * 5)).limit(5).toArray()
     console.log(result[0])
     // notice컬레션의 find 전체문서를 선택 /  findOne은 문서를 하나만 가져오겠다 
 
@@ -66,6 +84,74 @@ app.get('/list', async (req,res)=>{
     // 파라미터 넘기는 방법 list.ejs에 파리미터를 넘기겠다.
 })
 // list서버에 접속했을 때 실행 됨 -> result의 데이터의 0번이 실행되어야 하고, result는 notice의 
+
+
+//페이지네이션
+
+
+// 리뷰페이지 시작#############################################
+
+
+app.get('/review', async (req,res)=>{
+   
+    const result = await db.collection("review").find().toArray()
+   
+    res.render('review.ejs',{
+        data : result
+    })
+})
+
+app.post('/add', async(req,res)=>{
+   try{
+    await db.collection("review").insertOne({
+        title: req.body.title,
+        content: req.body.content
+
+    })
+   }catch(error){
+    console.log(error)
+   }
+   res.redirect('/review')
+})
+
+
+app.put('/reviewedit', async (req,res) =>{
+    console.log(res.body)
+    await db.collection("review").updateOne({
+        _id : new ObjectId(req.body._id)
+    },{
+        $set : {
+            title: req.body.title,
+            content: req.body.content
+        }
+    })
+    res.redirect('/review')
+})
+
+app.get('/reviewedit/:id' , async (req,res)=>{
+    const result = await db.collection("review").findOne({
+         _id : new ObjectId (req.params.id)
+    })
+    res.render('reviewedit.ejs',{
+         data : result
+    })
+ })
+
+
+ app.get('/delete/:id', async (req,res)=>{
+    await db.collection("review").deleteOne({
+     _id : new ObjectId (req.params.id)
+ })
+ res.redirect('/review')
+ })
+
+
+
+//이미지 넣어보기
+
+
+//리뷰 페이지 끝#########################################################################
+
 
 app.get('/portfolio', (req,res)=>{
     res.send("포트폴리오 페이지 수정됐음");
@@ -80,6 +166,70 @@ app.get('/view/:id', async (req,res)=>{
     })
     console.log(result)
 })
+
+app.get('/write', (req,res)=>[
+    res.render('write.ejs')
+])
+
+
+
+app.post('/add', async (req,res)=>{
+    console.log(req.body)
+    try{
+        console.log(req.body.params)
+    await db.collection("notice").insertOne({
+        title: req.body.title,
+        content: req.body.content
+    })
+    }catch(error){
+        console.log(error)
+    }
+    // res.send("성공!")
+    res.redirect('/list')
+})
+
+
+// 수정
+app.put('/edit', async (req,res)=>{
+    /* updateOne({문서},{
+        $set : {원하는 값: 변경 값}
+    })*/
+   
+    await db.collection("notice").updateOne({
+        _id : new ObjectId(req.body._id)
+    },{
+        $set : {
+            title: req.body.title,
+            content: req.body.content
+        }
+    })
+    // const result = "";
+    // res.send(result)
+    res.redirect('/list')
+})
+
+app.get('/edit/:id', async(req,res)=>{
+    const result = await db.collection("notice").findOne({
+        _id : new ObjectId (req.params.id)
+    })
+    res.render('edit.ejs',{
+        data : result
+    })
+})
+
+
+// 삭제
+app.get('/delete/:id', async (req,res)=>{
+   await db.collection("notice").deleteOne({
+    _id : new ObjectId (req.params.id) // :id와 동일한 data / 서로 일치할 경우 삭제한다. 
+})
+res.redirect('/list')
+})
+// send와 redirect는 함께 사용하지 못한다.
+
+
+
+
 /*
 1. Uniform Interface
 여러 URL과 Method는 일관성이 있어야 하며, 하나의 URL에서는 하나의 데이터만 가져오게 디자인하며,
@@ -90,7 +240,6 @@ tip
 2) 띄어쓰기는 언더바 대신 대시 기호
 3) 파일 확장자는 사용금지
 4) 하위 문서를 뜻할 땐, /기호를 사용
-
 
 2. 클라이언트와 서버역할 구분
 유저에게 서버 역할을 맡기거나 직접 입출력을 시키면 안된다.
@@ -107,7 +256,6 @@ tip
 6. Code on Demeand
 서버는 실행 가능한 코드를 보낼 수 있다.
 */ 
-
 
 //app.get 서버를 불러오는 것
 //req = request , res = response (결과값을 보내는 것)
@@ -128,9 +276,9 @@ tip
 5. 터미널을 추가 해준 후 npx tailwindcss -i ./page/css/tailwind.css -o ./public/index.css --watch 입력
 */
 
-
 /* await 은 함수안에서만 실행 async와 await은 promise라는  오브젝트를 반환한다.
    pendending , 성공 , 에러 
    pendending은 작업중인 상태
 */ 
 
+// 리액트의 경우 Mongoose를 사용하여야 한다. 
